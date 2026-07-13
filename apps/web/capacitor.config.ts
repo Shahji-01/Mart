2200df41c7b66db050717d0deb5044c3204be294
Shahji-01 <abhinavshah133@gmail.com>
2026-07-13 23:14:01 +0530

    fix: update capacitor config with custom hostname for cors

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'shankeswartraders.app',
   appName: 'Shankeswar Traders',
-  webDir: 'dist/public'
+  webDir: 'dist/public',
+  server: {
+    androidScheme: 'https',
+    hostname: 'mart.shahji.dev'
+  }
 };
 
 export default config;
